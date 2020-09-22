dce8fd443b809bb3893bd7614671a9d1d798632d
geeksarray <36873556+geeksarray@users.noreply.github.com>
2020-09-22 09:31:50 +0530

    initial files

---
+++ RenderPartialViews/RenderPartialViews/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RenderPartialViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult GetAddress()
+        {
+            return PartialView("_address"); 
+        }
+
+      
+        [ChildActionOnly]
+        public ActionResult GetAddressAction()
+        {
+            return PartialView("_address"); 
+        }
+
+        [ChildActionOnly]        
+        public ActionResult ActionWithParameter(string category)
+        {
+            if(category == "billing")
+                return PartialView("_billingaddress");
+
+            return PartialView("_shippingaddress");
+        }        
+        
+        
+        public PartialViewResult GetAddressForjQuery(string category)
+        {
+            return PartialView("_address");
+        }
+
+        
+    }
+}
---
+++ RenderPartialViews/RenderPartialViews/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RenderPartialViews.MvcApplication" Language="C#" %>
